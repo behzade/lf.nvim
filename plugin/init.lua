1da448330302d4e4fa4f107ba172edad95794100
Behzad Ehsani <ehsani.eof@gmail.com>
2024-03-14 15:27:12 +0330

    initial commit

---
+++ plugin/init.lua
@@ -0,0 +1,35 @@
+if vim.fn.exists('g:loaded_lf_hijack') ~= 0 then
+    return
+end
+
+vim.g.loaded_lf_hijack = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.cmd('autocmd! FileExplorer *')
+
+local function hijack_directory()
+    local path = vim.fn.expand('%:p')
+    if vim.fn.isdirectory(path) == 0 then
+        return
+    end
+
+    if type(path) == "table" then
+        path = path[0]
+    end
+
+    local bufnr = vim.fn.bufnr()
+    vim.cmd(string.format('keepjumps keepalt term lf  %s', vim.fn.fnameescape(path)))
+    vim.cmd(string.format('silent! bwipeout %d', bufnr))
+end
+
+vim.api.nvim_create_augroup('lf_hijack', { clear = true })
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+    group = 'lf_hijack',
+    pattern = { "*" },
+    nested = true,
+    callback = function()
+        hijack_directory()
+    end
+})
